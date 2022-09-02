9160586277293325208751b23355305e66362830
Tay811 <mataitaya@gmail.com>
2022-09-02 20:27:12 +0300

    Updates

---
+++ precache-manifest.ea834387025b109f135e045e9036b5ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae87f513718dc5175f2a",
+    "url": "/got_API/static/css/main.bae1b58a.chunk.css"
+  },
+  {
+    "revision": "ae87f513718dc5175f2a",
+    "url": "/got_API/static/js/main.ae87f513.chunk.js"
+  },
+  {
+    "revision": "df9f8a84e364460c2d71",
+    "url": "/got_API/static/css/1.c2bd772d.chunk.css"
+  },
+  {
+    "revision": "df9f8a84e364460c2d71",
+    "url": "/got_API/static/js/1.df9f8a84.chunk.js"
+  },
+  {
+    "revision": "0abd7dbfa89a1887383c",
+    "url": "/got_API/static/js/runtime~main.0abd7dbf.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/got_API/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/got_API/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "da47f672601610975ee2b143d1129bd1",
+    "url": "/got_API/index.html"
+  }
+];
